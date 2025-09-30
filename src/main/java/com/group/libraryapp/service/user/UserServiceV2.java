2caf7c1d3d47484a9459155cf84ee9353e333abd
Yulimindi <128598263+Yulimindi@users.noreply.github.com>
2025-09-30 18:35:22 +0900

    250929 섹션 5.생애 최초 JPA 사용하기

--- src/main/java/com/group/libraryapp/service/user/UserServiceV2.java
+++ src/main/java/com/group/libraryapp/service/user/UserServiceV2.java
@@ -6,6 +6,7 @@ import com.group.libraryapp.dto.user.request.UserCreateRequest;
 import com.group.libraryapp.dto.user.request.UserUpdateRequest;
 import com.group.libraryapp.dto.user.response.UserResponse;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 import java.util.stream.Collectors;
@@ -19,20 +20,23 @@ public class UserServiceV2 {
         this.userRepository = userRepository;
     }
 
-    public void saveUser(UserCreateRequest request) {
+    @Transactional // 아래 있는 함수가 시작될 때 start transcation을 해준다 (트랜잭션을 시작!), 함수가 예외 없이 잘 끝났다면 commit, 문제가 있다면 rollback
+    public void saveUser(UserCreateRequest request) { // 저장
         // save 메소드에 객체를 넣어주면 INSERT SQL이 자동으로 갈라감
         // save 된 후의 User는 id가 있다!
         userRepository.save(new User(request.getName(), request.getAge()));
     }
 
-    public List<UserResponse> getUsers() {
+    @Transactional(readOnly = true)
+    public List<UserResponse> getUsers() { // 조회
         return userRepository.findAll().stream()
                 .map(UserResponse::new)
                 .collect(Collectors.toList());
 
     }
 
-    public void updateUser(UserUpdateRequest request) {
+    @Transactional
+    public void updateUser(UserUpdateRequest request) { // 수정
         // select * from users where id = ?;
         // 결과 : Optional<User>
         User user = userRepository.findById(request.getId())
@@ -42,7 +46,8 @@ public class UserServiceV2 {
         userRepository.save(user);
     }
 
-    public void deleteUser(String name) {
+    @Transactional
+    public void deleteUser(String name) { // 삭제
         // SELECT * FROM user WHERE name = ?;
         User user = userRepository.findByName(name); // find라고 작성하면 1개만 들고 옴, By 뒤에 붙는 필드 이름으로 SELECT 쿼리의 WHERE문이 작성됨
         if (user == null) {
